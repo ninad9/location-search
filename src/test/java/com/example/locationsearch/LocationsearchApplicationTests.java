11d7bf26c1473dc09b6573a23269d9087d4086d5
ninad9 <78981903+ninad9@users.noreply.github.com>
2025-09-30 23:53:40 +0200

    initial commit

---
+++ src/test/java/com/example/locationsearch/LocationsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.locationsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
